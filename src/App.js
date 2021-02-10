7d8901fad64bb0790340221f7126f8f781b25f28
IshanJawade <ishankjawade@gmail.com>
2021-02-10 18:12:34 +0530

    UI is done

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import React from 'react';
 const api = {
   key: "0b51c0d391ee29559db5bf2466be75b8",
   base: "https://api.openweathermap.org/data/2.5",
-}
+};
+
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${date} ${month} ${year}`
+  }
+
   return (
     <div className="app">
       <main>
@@ -20,8 +34,18 @@ function App() {
         <div>
           <div className='location-box'>
             <div className="location">Pune, Maharashtra, India</div>
-            <div className="date">10/02/2021</div>
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
+
+          <div className='weather-box'>
+            <div className='temp'>
+              15°C
+            </div>
+            <div className="weather">
+              Sunny
+            </div>
+          </div>
+
         </div>
       </main>
     </div>
